e88d47b0ea6ae092a2d310e9314202b51c629b7d
snowbowls <geewiztizzy@gmail.com>
2022-02-18 13:15:13 -0500

    initial

---
+++ src/main/java/bot.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class bot {
+
+    public static void main(String[] args) throws Exception{
+
+        JDABuilder builder = JDABuilder.createDefault("OTQ0MjQ3NjU4NTUyMzg1NTY2.Yg-1KQ.4ki6xUbKh7o_VlXHrKppWQZPYJY");
+        builder.build();
+
+    }
+}
