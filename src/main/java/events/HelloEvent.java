85083cd9571491a78f2d90b4709b27f0d4f0bbdf
snowbowls <geewiztizzy@gmail.com>
2022-02-22 13:45:33 -0500

    plus15 started

--- src/main/java/events/HelloEvent.java
+++ src/main/java/events/HelloEvent.java
@@ -8,7 +8,11 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class HelloEvent extends ListenerAdapter {
     public void onMessageReceived(MessageReceivedEvent event)
     {
-        System.out.printf("[%s]: %s\n", event.getAuthor().getName(), event.getMessage().getContentDisplay());
+       // System.out.printf("[%s]: %s\n", event.getAuthor().getName(), event.getMessage().getContentDisplay());
+       // System.out.printf("Server: [%s]\n", event.getGuild().getId());
+        if(event.getGuild().getId().equals(("944254135476305980"))){
+            System.out.print("SUC\n");
+        }
         //event.getMessage().addReaction("boatyvv:854825460494761994").queue();
     }
 }
--- src/main/java/events/plusEvent.java
+++ src/main/java/events/plusEvent.java
@@ -1,25 +1,23 @@
 package events;
 
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.ErrorResponse;
 
 public class plusEvent extends ListenerAdapter {
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        if(event.getGuild().getId().equals(("944254135476305980"))) {
+            String id = event.getMessageId();
 
-
-        if(event.getReactionEmote().getId().equals("900119408859578451")){
-           // event.getChannel().sendMessage("YEEET").queue();
-            System.out.printf("%s\n", event.getChannel());
-
-
+            if (event.getReactionEmote().getId().equals("900119408859578451")) {
+                event.getChannel().retrieveMessageById(id).queue((message) -> { // async callback
+                    System.out.printf("[%s]: %s\n", message.getAuthor().getName(), message.getContentDisplay());
+                });
+            }
         }
-        //event.getChannel().sendMessage("U+1F44D").complete();
-
-        //event.getChannel().addReactionby(":boatyvv:854825460494761994");
-        //event.getChannel().sendMessage(embedBuilder.build()).complete().addReaction("✔").queue();
-
-
     }
 }
