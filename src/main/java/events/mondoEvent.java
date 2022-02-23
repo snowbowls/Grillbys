7b61c270404f99152659b7b7531d6a5d28f51f0a
snowbowls <geewiztizzy@gmail.com>
2022-02-23 16:22:33 -0500

    mondodb testing

--- src/main/java/events/mondoEvent.java
+++ src/main/java/events/mondoEvent.java
@@ -6,13 +6,19 @@ import com.mongodb.client.MongoDatabase;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bson.Document;
+import org.jetbrains.annotations.NotNull;
 
 public class mondoEvent extends ListenerAdapter {
-    public void onMessageReceived(MessageReceivedEvent event){
-        MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://snowbowls:13241324@cluster0.r4aqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"));
+
+    public static final String MONDODB = System.getenv("MONDODB");
+
+    public void onMessageReceived(@NotNull MessageReceivedEvent event){
+
+        MongoClient mongoClient = new MongoClient(new MongoClientURI(MONDODB));
         MongoDatabase database = mongoClient.getDatabase("sample_restaurants");
         MongoCollection<Document> collection = database.getCollection("sample_restaurants.restaurants\n");
         Document student1 = collection.find(new Document("cuisine", "American")).first();
+
        // System.out.println("Student 1: " + student1.toJson()); // IT BREAKS HERE
 
 
