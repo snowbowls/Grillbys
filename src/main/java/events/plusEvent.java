799ce6edd31ac8aea3df6477e20d5eeb7fe92b3a
snowbowls <geewiztizzy@gmail.com>
2022-02-23 20:59:32 -0500

    mongodb ret values

--- src/main/java/events/plusEvent.java
+++ src/main/java/events/plusEvent.java
@@ -1,11 +1,13 @@
 package events;
 
+import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.Sorts;
+import com.mongodb.client.result.InsertOneResult;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -15,6 +17,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.ErrorResponse;
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -22,25 +25,30 @@ public class plusEvent extends ListenerAdapter {
     public static final String uri = System.getenv("URI");
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
 
-        Message msg = event.getChannel().retrieveMessageById(event.getMessageId()).complete();
-        String username;
-        //username = msg.getAuthor().getName();
-        //System.out.printf("%s ", username);
+        String username = event.retrieveMessage().complete().getAuthor().getName();
 
-        /*try (MongoClient mongoClient = MongoClients.create(uri)) {
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
             MongoDatabase database = mongoClient.getDatabase("ChillGrill");
             MongoCollection<Document> collection = database.getCollection("socialcredit");
             Bson projectionFields = Projections.fields(
-                    Projections.include("user"),
+                    Projections.include("user", "score"),
                     Projections.excludeId());
-            Document doc = collection.find(eq("title", username))
+            Document doc = collection.find(eq("user", username))
                     .projection(projectionFields)
                     .first();
             if (doc == null) {
-                System.out.println("No results found.");
+                try {
+                    InsertOneResult result = collection.insertOne(new Document()
+                            .append("_id", new ObjectId())
+                            .append("user", username)
+                            .append("score", 0));
+                    System.out.println("Success! Inserted document id: " + result.getInsertedId());
+                } catch (MongoException me) {
+                    System.err.println("Unable to insert due to an error: " + me);
+                }
             } else {
                 System.out.println(doc.toJson());
             }
-        }*/
+        }
     }
 }
