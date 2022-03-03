73eeca662ba7e43e14f2b113138875410437575a
snowbowls <geewiztizzy@gmail.com>
2022-03-02 19:43:15 -0500

    tag event work

--- src/main/java/events/TagUserEvent.java
+++ src/main/java/events/TagUserEvent.java
@@ -1,7 +1,15 @@
 package events;
 
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class TagUserEvent extends ListenerAdapter {
+    public void onMessageReceived(MessageReceivedEvent event) {
+        String msg = event.getMessage().getContentRaw().toLowerCase();
 
+        if(event.getMessage().getAuthor().getId().equals("222163619125788682")){
+            event.getChannel().sendMessage("<@222163619125788682>").complete();
+            //event.getChannel().getHistory().
+        }
+    }
 }
