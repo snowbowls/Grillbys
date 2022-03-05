3ea2d826c27f22b6af7dba15fc27333ffc516944
snowbowls <geewiztizzy@gmail.com>
2022-03-05 18:54:10 -0500

    refining

--- src/main/java/events/JonEvent.java
+++ src/main/java/events/JonEvent.java
@@ -10,13 +10,13 @@ public class JonEvent extends ListenerAdapter {
         String msg = event.getMessage().getContentRaw().toLowerCase();
 
         final Boolean[] trig = {false};
-        ArrayList<String> triggers = new ArrayList<>();
-        triggers.add("dbs");
-        triggers.add("dragonball");
-        triggers.add("goku");
-        triggers.add("bejita");
+        ArrayList<String> triggersDBS = new ArrayList<>();
+        triggersDBS.add("dbs");
+        triggersDBS.add("dragonball");
+        triggersDBS.add("goku");
+        triggersDBS.add("bejita");
 
-        triggers.forEach((x) -> {
+        triggersDBS.forEach((x) -> {
             if(msg.contains(x)){
                 trig[0] = true;
             }
@@ -24,12 +24,8 @@ public class JonEvent extends ListenerAdapter {
 
         if(trig[0]){
             event.getChannel().sendMessage("**SHUTUPSHUTUPSHUTUPSHUTUPSHUTUP**").queue();
-            if(event.getGuild().getId().equals("816125354875944960")) {
-                event.getMessage().addReaction("15_neg:934919187787288597").queue();
-            }
-            else{
-                event.getMessage().addReaction("amongass:854818205624827935").queue();
-            }
+
+            event.getMessage().addReaction("15_neg:934919187787288597").queue();
         }
 
         if(msg.equals("jiren") && !trig[0]){
@@ -39,6 +35,26 @@ public class JonEvent extends ListenerAdapter {
         if(msg.equals("moe") && !trig[0]){
             event.getChannel().sendMessage("moe").queue();
         }
+
+        final Boolean[] trig2 = {false};
+        ArrayList<String> triggersRDM = new ArrayList<>();
+        triggersRDM.add("sus");
+        triggersRDM.add("us");
+        triggersRDM.add("among");
+        triggersRDM.add("amungus");
+        triggersRDM.add("amogus");
+        triggersRDM.add("imposter");
+
+        triggersRDM.forEach((x) -> {
+            if(msg.contains(x)){
+                trig2[0] = true;
+            }
+        });
+
+        if(trig2[0]) {
+            event.getMessage().addReaction("amongass:854818205624827935").queue();
+        }
+
 
     }
 }
